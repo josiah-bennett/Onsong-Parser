d2d30fa94a3354be25e537d8eb7ef545d122ef14
josiah-bennett <josiah.bennett@web.de>
2022-03-14 17:35:52 +0100

    added spacing for chords which overlap due to css formatting, 3 tests are failing as of now and tests are still incomplete.

--- src/Internal/Html.hs
+++ src/Internal/Html.hs
@@ -3,12 +3,25 @@
 {-# LANGUAGE OverloadedStrings    #-}
 
 module Internal.Html
-    ( interleave
-    , chord
+    ( checkChordSpacing
+    , interleave
+    , chord_
     , line
     ) where
 
+import           Prelude hiding (length)
 import           Lucid
+import           Data.Text (Text, unpack, pack, length)
+
+
+checkChordSpacing :: [Text] -> [Text] -> [Int]
+checkChordSpacing []  [c] = []
+checkChordSpacing [t] [c] = []
+checkChordSpacing [t] [] = []
+checkChordSpacing [t1,t2] [c] = []
+checkChordSpacing (_:t:ts) (c1:c2:cs) 
+    | length c1 >= length t = (14 * (length c1 - length t)):checkChordSpacing (t:ts) (c2:cs) 
+    | otherwise = 0:checkChordSpacing (t:ts) (c2:cs)
 
 
 interleave :: [a] -> [a] -> [a]
@@ -17,12 +30,19 @@ interleave []     bs     = bs
 interleave (a:as) (b:bs) = a:b:interleave as bs
 
 
-chord :: ToHtml a => a -> Html ()
-chord c = span_ [class_ "chord"] (toHtml c)
+chord_ :: Term a r => a -> r
+chord_ = termWith "span" [class_ "chord"]
 
---line :: ToHtml a => [a] -> [a] -> Html ()
+
+addPadding :: Int -> Text -> Html ()
+addPadding padding chord | padding == 0 = chord_ (toHtml chord) 
+                         | otherwise    = chord_ [style_ pad] (toHtml chord)
+        where pad = pack ("padding-left: " ++ show padding ++ ";")
+
+
+line :: [Text] -> [Text] -> Html ()
 line [] [] = ""
 line textPieces chords = mconcat (interleave htmlText htmlChords ++ [br_ []])
-    where htmlText   = map toHtml textPieces
-          htmlChords = map chord chords
-
+    where padding    = 0:checkChordSpacing textPieces chords
+          htmlText   = map toHtml textPieces
+          htmlChords = map (uncurry addPadding) (zip padding chords)
--- test/HtmlSpec.hs
+++ test/HtmlSpec.hs
@@ -9,6 +9,7 @@ import           Internal.Html
 import           Test.Hspec
 
 import           Lucid         (renderText)
+import           Data.Text     (Text)
 
 spec :: Spec
 spec = do
@@ -86,13 +87,13 @@ chordSpec :: Spec
 chordSpec = describe "Tests the function responsible for creating the html chord structure" $ do
   context "When no chord is passed" $
     it "should generate an empty span tag" $
-      renderText (chord "") `shouldBe` "<span class=\"chord\"></span>"
+      renderText (chord_ "") `shouldBe` "<span class=\"chord\"></span>"
   context "When a simple chord is passed" $
     it "should generate a simple chord" $
-      renderText (chord "G") `shouldBe` "<span class=\"chord\">G</span>"
+      renderText (chord_ "G") `shouldBe` "<span class=\"chord\">G</span>"
   context "When a complicated chord is passed" $
     it "should generate a complicated chord" $
-      renderText (chord "Gsus4maj7add9/B") `shouldBe` "<span class=\"chord\">Gsus4maj7add9/B</span>"
+      renderText (chord_ "Gsus4maj7add9/B") `shouldBe` "<span class=\"chord\">Gsus4maj7add9/B</span>"
 
 
 -- I think I got all of the test cases here ...
@@ -100,27 +101,27 @@ lineSpec :: Spec
 lineSpec = describe "Tests the function that takes a list of song text pieces and chords and creates a usable html formatted line" $ do
   context "When no data is passed" $
     it "should return no data" $
-      renderText (line ([] :: [String]) ([] :: [String])) `shouldBe` ""
+      renderText (line ([] :: [Text]) ([] :: [Text])) `shouldBe` ""
   describe "When no song pieces are available" $ do
     context "When an empty chord is passed" $
       it "should generate an empty span tag with a line break" $
-        renderText (line ([] :: [String]) [""]) `shouldBe` "<span class=\"chord\"></span><br>"
+        renderText (line ([] :: [Text]) [""]) `shouldBe` "<span class=\"chord\"></span><br>"
     context "When a single chord is passed" $
       it "should generate a span tag with the chord and then a line break" $
-        renderText (line ([] :: [String]) ["G"]) `shouldBe` "<span class=\"chord\">G</span><br>"
+        renderText (line ([] :: [Text]) ["G"]) `shouldBe` "<span class=\"chord\">G</span><br>"
     context "When multiple chords are passed" $
       it "should generate multiple span tags with the chords and end with a line break" $
-        renderText (line ([] :: [String]) ["C","D7","G"]) `shouldBe` "<span class=\"chord\">C</span><span class=\"chord\">D7</span><span class=\"chord\">G</span><br>"
+        renderText (line ([] :: [Text]) ["C","D7","G"]) `shouldBe` "<span class=\"chord\">C</span><span class=\"chord\">D7</span><span class=\"chord\">G</span><br>"
   describe "When no chords are available" $ do
     context "When an empty string is passed" $
       it "should generate just a line break" $
-        renderText (line [""] ([] :: [String])) `shouldBe` "<br>"
+        renderText (line [""] ([] :: [Text])) `shouldBe` "<br>"
     context "When a single text piece is passed" $
       it "should generate that text with a line break" $
-        renderText (line ["some lyrics"] ([] :: [String])) `shouldBe` "some lyrics<br>"
+        renderText (line ["some lyrics"] ([] :: [Text])) `shouldBe` "some lyrics<br>"
     context "When lots of text is passed" $
       it "should write out all of the lyrics and end with a line break" $
-        renderText (line ["some lyrics", " and ", "some more lyrics"] ([] :: [String])) `shouldBe` "some lyrics and some more lyrics<br>"
+        renderText (line ["some lyrics", " and ", "some more lyrics"] ([] :: [Text])) `shouldBe` "some lyrics and some more lyrics<br>"
   describe "When both chords and lyrics are available" $ do
     context "When a 'normal' line is passed" $
       it "should generate a 'normal' line" $
